07d4694cdd0b4aa2fadf5ea08cb03a5b5be5648b
Mathias Lengler <mathmatze@googlemail.com>
2017-12-15 03:45:15 +0100

    restructure benchmark

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,53 @@
+extern crate csv;
+
+use std::error::Error;
+
+
+pub fn parse_test() -> Result<(), Box<Error>> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b'\t')
+        .has_headers(false)
+        .quoting(false)
+        .comment(Some(b'#'))
+        .from_path("../database/dictcc_DE-EN.txt")?;
+
+    let mut vec = vec![];
+
+    for (i, record) in rdr.records().enumerate() {
+        let record = match record {
+            Ok(record) => record,
+            Err(err) => {
+                let abort = {
+                    let kind: &csv::ErrorKind = err.kind();
+
+                    match *kind {
+                        csv::ErrorKind::UnequalLengths { .. } => false,
+                        _ => true,
+                    }
+                };
+
+
+                if abort {
+                    return Err(Box::new(err));
+                } else {
+                    eprintln!("Dictionary Parsing Error: {}", err);
+                    continue;
+                }
+            }
+        };
+
+        if i % 1000 == 0 {
+            println!("{:?}", record);
+        }
+
+        vec.push(record);
+    }
+
+    eprintln!("vec.len() = {:?}", vec.len());
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,5 @@
-extern crate csv;
-
-use std::error::Error;
-
-fn parse_test() -> Result<(), Box<Error>> {
-    let mut rdr = csv::ReaderBuilder::new()
-        .delimiter(b'\t')
-        .has_headers(false)
-        .quoting(false)
-        .comment(Some(b'#'))
-        .from_path("../database/dictcc_DE-EN.txt")?;
-
-    let mut vec = vec![];
-
-    for (i, record) in rdr.records().enumerate() {
-        let record = match record {
-            Ok(record) => record,
-            Err(err) => {
-                let abort = {
-                    let kind: &csv::ErrorKind = err.kind();
-
-                    match *kind {
-                        csv::ErrorKind::UnequalLengths { .. } => false,
-                        _ => true,
-                    }
-                };
-
-
-                if abort {
-                    return Err(Box::new(err));
-                } else {
-                    eprintln!("Dictionary Parsing Error: {}", err);
-                    continue;
-                }
-            }
-        };
-
-        if i % 1000 == 0 {
-            //println!("{:?}", record);
-        }
-
-        vec.push(record);
-    }
-
-    std::io::stdin().read_line(&mut String::new());
-
-    Ok(())
-}
+extern crate dictcc;
 
 fn main() {
-    parse_test().unwrap();
+    dictcc::parse_test().unwrap();
 }
